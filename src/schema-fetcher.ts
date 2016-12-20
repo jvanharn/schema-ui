ccf312368a78a6392517e960ff7dcdc256f3d31b
Jeffrey H <j.harn@smartphonehoesjes.nl>
2016-12-20 12:02:40 +0100

    Adds an schema fetcher interface, which defines the contract for services to fetch new schemas from a server, based on their identity.

---
+++ src/schema-fetcher.ts
@@ -0,0 +1,17 @@
+import { JsonSchema } from './models/index';
+
+/**
+ * Schema retrieval interface.
+ *
+ * Interface that makes it possible to request an schema from the original endpoint it should be taken from, by id.
+ */
+export interface ISchemaFetcher {
+    /**
+     * Fetch the given schema by it's identifier.
+     *
+     * @param id The schema identifier for the schema to retrieve.
+     *
+     * @return A promise that resolves to the requested schema, or is rejected if it was not found.
+     */
+    fetchSchema(id: string): Promise<JsonSchema>;
+}
