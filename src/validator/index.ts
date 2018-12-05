df3171f53f3a99fecaf51c0b869f9d2daa4bdfb9
Jeffrey H <j.harn@smartphonehoesjes.nl>
2018-12-05 17:54:27 +0100

    Forgot to include in index.

--- src/validator/index.ts
+++ src/validator/index.ts
@@ -1,3 +1,3 @@
 export { ISchemaValidator, ValidationResult, ValidationError } from './schema-validator';
 export { AjvSchemaValidator, AjvSchemaValidator as SchemaValidator } from './ajv-schema-validator';
-export { ValidatorCache } from './validator-cache';
+export { ValidatorCache, createAjvValidatorCache } from './validator-cache';
