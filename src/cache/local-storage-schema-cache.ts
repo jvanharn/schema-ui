bda89f69dbfdb37903b8b798d4efdc4700796e55
Jeffrey H <j.harn@smartphonehoesjes.nl>
2019-11-26 15:23:29 +0100

    Improve local storage schema-cache by no longer requireing the root-schemalist and making it fully evented.

--- src/cache/local-storage-schema-cache.ts
+++ src/cache/local-storage-schema-cache.ts
@@ -1,5 +1,4 @@
 import { JsonSchema } from '../models/schema';
-import { CommonJsonSchema } from '../models/common';
 import { ISchemaCache } from './schema-cache';
 
 import * as _ from 'lodash';
@@ -35,9 +34,14 @@ const schemaCacheBucketRootList = 'root';
  */
 export class LocalStorageSchemaCache implements ISchemaCache {
     /**
+     * The registered event listener.
+     */
+    private readonly registeredBoundEventListener = this.localStorageSchemaCacheChangeListener.bind(this);
+
+    /**
      * Index of all schemas.
      */
-    private schemas: SchemaCacheEntry[] = [];
+    private schemas: { [key: string]: JsonSchema } = Object.create(null);
 
     /**
      * Checks whether or not localstorage is available on the current platform.
@@ -52,16 +56,7 @@ export class LocalStorageSchemaCache implements ISchemaCache {
 
         // Try and register an event listener, for when the cache changes
         try {
-            const that = this;
-            const rootListKey = this.prefixStorageProperty(schemaCacheBucketRootList);
-            window.addEventListener('storage', function localStorageSchemaCacheChangeListener(evt) {
-                if (evt.url === window.document.URL) {
-                    return;
-                }
-                if (evt.key === rootListKey) {
-                    that.loadSchemaList();
-                }
-            });
+            window.addEventListener('storage', this.registeredBoundEventListener);
         }
         catch (err) {
             debug('[warn] unable to register for storage change events:', err);
@@ -69,19 +64,82 @@ export class LocalStorageSchemaCache implements ISchemaCache {
     }
 
     /**
+     * Dispose of this schema cache.
+     */
+    public dispose(): void {
+        if (this.schemas == null) {
+            return;
+        }
+
+        try {
+            window.removeEventListener('storage', this.registeredBoundEventListener);
+        }
+        catch (err) {
+            debug('[warn] unable to deregister the storage event listener');
+        }
+        this.schemas = null;
+    }
+
+    /**
+     * Event listener that listens to all changed storage keys (by ourselves or others).
+     *
+     * @param evt
+     */
+    private localStorageSchemaCacheChangeListener(evt: StorageEvent): void {
+        // Somebody else changed the schema, (re)set it for next load!
+        if (this.hasCorrectBucketPrefix(evt.key)) {
+            var unprefixedKey = this.unprefixStorageProperty(evt.key);
+            if (evt.newValue != null) {
+                if (evt.url !== window.document.URL) {
+                    this.schemas[unprefixedKey] = null;
+                }
+                else if (!this.schemas[unprefixedKey]) {
+                    // User manually editted the schema?
+                    debug(`[warn] user possibly manually editted a schema by key "${unprefixedKey}"!`);
+                    this.schemas[unprefixedKey] = null;
+                }
+            }
+            else {
+                delete this.schemas[unprefixedKey];
+            }
+        }
+    }
+
+    /**
      * Get a json-schema by id.
      *
-     * @param id Schema id of the referenced schema.
+     * @param schemaId Schema id of the referenced schema.
      *
      * @return string
      */
-    public getSchema(id: string): JsonSchema {
-        if (!Array.isArray(this.schemas)) {
+    public getSchema(schemaId: string): JsonSchema {
+        if (this.schemas == null) {
             return null;
         }
 
-        const lowerId = id.toLowerCase();
-        return this.getEntry(this.schemas.find(x => String(x.id).toLowerCase() === lowerId));
+        const findKey = this.getSchemaNameForSchemaId(schemaId);
+        if (!(findKey in this.schemas)) {
+            return null;
+        }
+
+        // Key exists, and already cached
+        if (this.schemas[findKey] !== null) {
+            return this.schemas[findKey];
+        }
+
+        // Key exists, but not yet retrieved.
+        const storageKey = this.prefixStorageProperty(findKey);
+        try {
+            return this.schemas[findKey] = JSON.parse(platformStorage.getItem(storageKey));
+        }
+        catch (err) {
+            debug(`[warn] whilst trying to retrieve schema by id "${schemaId}" -> key(${findKey}), parsing of JSON failed:`, err);
+
+            delete this.schemas[findKey];
+            platformStorage.removeItem(storageKey);
+
+            return null;
+        }
     }
 
     /**
@@ -94,37 +152,26 @@ export class LocalStorageSchemaCache implements ISchemaCache {
             throw new Error(`LocalStorageSchemaCache.setSchema: Cannot cache the given schema; the schema or it's id is not set.`);
         }
 
+        const key = this.getSchemaNameForSchemaId(schema.id);
+
         // Create entry
-        var entry: SchemaCacheEntry = {
-            id: schema.id,
-            name: this.getSchemaNameForSchemaId(schema.id)
-        };
-        this.schemas.push(entry);
-
-        // Save the listing
-        this.saveSchemaList();
-
-        // Save the schema in cache.
-        this.setEntry(entry, schema);
+        this.schemas[key] = schema;
+        platformStorage.setItem(this.prefixStorageProperty(key), JSON.stringify(schema));
     }
 
     /**
      * @inherit
      */
-    public removeSchema(id: string): void {
-        if (!Array.isArray(this.schemas)) {
+    public removeSchema(schemaId: string): void {
+        if (this.schemas == null) {
             return;
         }
 
+        const key = this.getSchemaNameForSchemaId(schemaId);
+
         // Remove the schema itself
-        const lowerId = id.toLowerCase();
-        this.clearEntry(this.schemas.find(x => String(x.id).toLowerCase() === lowerId));
-
-        // Remove the entry from the schemas root listing
-        this.schemas = this.schemas.filter(x => x.id != lowerId);
-
-        // Save the listing
-        this.saveSchemaList();
+        delete this.schemas[key];
+        platformStorage.removeItem(this.prefixStorageProperty(key));
     }
 
     /**
@@ -135,10 +182,17 @@ export class LocalStorageSchemaCache implements ISchemaCache {
      * @return JsonSchema
      */
     public getSchemaBy(predicate: (schema: JsonSchema) => boolean): JsonSchema {
-        for (const entry of this.schemas) {
-            const schema = this.getEntry(entry);
-            if (predicate(schema)) {
-                return schema;
+        for (const key in this.schemas) {
+            if (Object.prototype.hasOwnProperty.call(this.schemas, key)) {
+                try {
+                    let schema = JSON.parse(platformStorage.getItem(this.prefixStorageProperty(key)));
+                    if (predicate(schema)) {
+                        return schema;
+                    }
+                }
+                catch (err) {
+                    debug(`[warn] while trying to iterate all schemas, one schema by key "${key}" could not be retrieved from the cache!`);
+                }
             }
         }
         return null;
@@ -152,125 +206,61 @@ export class LocalStorageSchemaCache implements ISchemaCache {
      * @param predicate The operation that should be executed for every schema in the cache.
      */
     public each(predicate: (schema: JsonSchema) => void): void {
-        for (const entry of this.schemas) {
-            const schema = this.getEntry(entry);
-            if (schema != null) {
-                predicate(schema);
-            }
-            else {
-                this.clearEntry(entry);
-            }
-        }
+        this.getSchemaBy(schema => {
+            predicate(schema);
+            return false;
+        });
     }
 
     /**
      * Clear all cached entries.
      */
     public clear(): void {
-        if (!Array.isArray(this.schemas)) {
-            this.schemas = [];
+        if (!this.schemas || typeof this.schemas !== 'object') {
+            this.schemas = { };
             return;
         }
 
-        // Clear all items that are set in the root list
-        for (const entry of this.schemas) {
-            this.clearEntry(entry);
-        }
+        try {
+            // Clear all items that are set in the root list
+            for (const key in this.schemas) {
+                if (Object.prototype.hasOwnProperty.call(this.schemas, key)) {
+                    platformStorage.removeItem(this.prefixStorageProperty(key));
+                }
+            }
 
-        // Clear items that slipped out by manual editing or desynchronisation between tabs.
-        for (var i = 0; i < platformStorage.length; i++) {
-            let key = platformStorage.key(i);
-            if (key.substr(0, schemaCacheBucketName.length) === schemaCacheBucketName) {
-                platformStorage.removeItem(key);
+            // Clear items that slipped out by manual editing or desynchronisation between tabs.
+            for (var i = 0; i < platformStorage.length; i++) {
+                let key = platformStorage.key(i);
+                if (this.hasCorrectBucketPrefix(key)) {
+                    platformStorage.removeItem(key);
+                }
             }
         }
+        catch (err) {
+            debug(`[warn] unable to successfully clear all the schemas:`, err);
+        }
 
-        this.schemas = [];
-        this.saveSchemaList();
+        this.schemas = { };
     }
 
     //region internal methods
         /**
-         * Get an JsonSchema by an entry.
-         */
-        private getEntry(entry?: SchemaCacheEntry): JsonSchema | null {
-            if (entry == null) {
-                return null;
-            }
-
-            if (entry.schema != null) {
-                return entry.schema;
-            }
-
-            var result = platformStorage.getItem(this.prefixStorageProperty(entry.name));
-            if (result == null) {
-                return null;
-            }
-
-            return entry.schema = JSON.parse(result);
-        }
-
-        /**
-         * Set entry in cache. (Wont update listing)
-         */
-        private setEntry(entry: SchemaCacheEntry, schema: JsonSchema): void {
-            platformStorage.setItem(this.prefixStorageProperty(entry.name), JSON.stringify(schema));
-        }
-
-        /**
-         * Remove an entry from cache.
-         */
-        private clearEntry(entry?: SchemaCacheEntry | SchemaCacheEntry[]): void {
-            if (Array.isArray(entry) && entry.length > 0) {
-                for (var item of entry) {
-                    if (!!item.name) {
-                        platformStorage.removeItem(item.name);
-                    }
-                }
-            }
-            else if (!!entry && !!(<SchemaCacheEntry>entry).name) {
-                platformStorage.removeItem((<SchemaCacheEntry>entry).name);
-            }
-        }
-
-        /**
-         * Saves the current rootlist (this.schemas) to the cache.
-         */
-        private saveSchemaList(): void {
-            const rootListKey = this.prefixStorageProperty(schemaCacheBucketRootList);
-            try {
-                const rootSchemaList = this.schemas.map(x => x.id);
-                platformStorage.setItem(rootListKey, JSON.stringify(rootSchemaList));
-            }
-            catch (e) {
-                // If something goes wrong, just clear the cache to be sure everything stays working.
-                debug('[warn] couldnt cache the schema list, something went wrong (force cleared schemas root list to be shure):', e);
-                this.schemas = [];
-            }
-        }
-
-        /**
          * Loads the schemas from cache.
          */
         private loadSchemaList(): void {
-            const rootListKey = this.prefixStorageProperty(schemaCacheBucketRootList);
-            debug(`loading root schema list from `);
+            debug(`check local/sessionStorage for schemas within our bucket named "${schemaCacheBucketName}"`);
+
             try {
-                var result = platformStorage.getItem(rootListKey);
-                if (!!result) {
-                    const rootSchemaList: any[] = JSON.parse(result);
-                    this.schemas = rootSchemaList.map(item => {
-                        if (typeof item === 'string') {
-                            return { id: item, name: this.getSchemaNameForSchemaId(item) };
-                        }
-                        else {
-                            return item;
-                        }
-                    });
+                for (var i = 0; i < platformStorage.length; i++) {
+                    let key = platformStorage.key(i);
+                    if (this.hasCorrectBucketPrefix(key)) {
+                        this.schemas[this.unprefixStorageProperty(key)] = null;
+                    }
                 }
             }
             catch (err) {
-                debug(`[warn] something went wrong trying to reet the root schema cache list from key "${rootListKey}":`, err);
+                debug(`[warn] something went wrong trying to read the cached schemas:`, err);
                 this.clear();
             }
         }
@@ -283,29 +273,24 @@ export class LocalStorageSchemaCache implements ISchemaCache {
         }
 
         /**
+         * Prefixes the given name according to the bucket name.
+         */
+        private unprefixStorageProperty(name: string): string {
+            return name.substr(schemaCacheBucketName.length + 1);
+        }
+
+        /**
+         * Check whether the given key is contained within the correct bucket.
+         */
+        private hasCorrectBucketPrefix(key: string): boolean {
+            return key.substr(0, schemaCacheBucketName.length) === schemaCacheBucketName;
+        }
+
+        /**
          * Get the schema name by taking the last part of the schema id.
          */
         private getSchemaNameForSchemaId(schemaId: string): string {
-            return _.kebabCase(schemaId.split('/').slice(2).join('-'));
+            return _.kebabCase(schemaId.toLowerCase().split('/').slice(2).join('-'));
         }
     //endregion
-}
-
-interface SchemaCacheEntry {
-    /**
-     * Identity of the schema.
-     */
-    id: string;
-
-    /**
-     * Name of the schema.
-     */
-    name: string;
-
-    /**
-     * If already loaded in memory once, it is stored here, so we dont continue working on copies.
-     *
-     * (Reduces memory usage)
-     */
-    schema?: JsonSchema;
 }
